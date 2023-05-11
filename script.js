2a2e88d64b41f2741ab1ec91de7fee4b9a152a73
ksy809 <ss741219@daum.net>
2023-05-11 19:15:48 +0900

    index
    자바스크립트..,.,. console.log랑 변수 만들기... 경고창 만들기... 연습했습니다...

---
+++ script.js
@@ -0,0 +1,11 @@
+alert("hi");
+
+const a = 5;
+const b = 3;
+const myName = "시연";
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
