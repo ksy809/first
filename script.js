eee93426683094a77ef19662bc72f1f8883a83c2
ksy809 <ss741219@daum.net>
2023-05-14 17:20:29 +0900

    array
    크롬 창에서 왜 안 뜨지??

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ console.log(a - b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello " + myName);
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// Get Item form Array
+console.log(daysOfWeek[4]);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+
+//object 만들기
+const player = {
+  name: "nico",
+  points: 10,
+  fat: true,
+};
